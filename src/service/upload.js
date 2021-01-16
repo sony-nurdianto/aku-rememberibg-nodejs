1493dedd361c9ebe2279ad737b906b40bff318a1
sony-nurdianto <sonynurdianto445@gmail.com>
2021-01-16 10:11:02 +0700

    fixing split base64 encode

--- src/service/upload.js
+++ src/service/upload.js
@@ -1,11 +1,16 @@
 import fs from 'fs'
 
+// src/assets/member_images/5870000647_5dd74ec903b57c5773e4f596.png
 
 class Upload {
 
     photoUpload = (no, id, photo) => {
 
-        const buff = Buffer.from(photo, 'base64')
+        const image = photo.split('base64,')[1]
+        if (!image) {
+            image = photo
+        }
+        const buff = Buffer.from(image, 'base64')
 
         try {
             fs.writeFileSync(`src/assets/member_images/${no}_${id}.png`, buff);
